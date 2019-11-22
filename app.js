eb7075c3ae6d771874040c2e610b79de13d4ccf7
Roger Barrow <46138186+rogerbarrow@users.noreply.github.com>
2019-11-21 20:56:03 -0600

    reverseString

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-function reverseString(str) {
+function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
+////////////////////////////////////////////////////////////////////////////////
+function reverse(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+///////////////////////////////////////////////////////////////////////
+function reverse(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+/////////////////////////////////////////////////////////////////////////
 
-  return str.split("").reverse().join("");
-
-
-  console.log(reverseString("hello"));
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
