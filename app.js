ff35ac381df60fa1cde7f1afd0ce596d45e760bd
Roger Barrow <46138186+rogerbarrow@users.noreply.github.com>
2019-11-25 23:39:09 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-function reverse(str) {
-  const arr = str.split('');
-  arr.reverse();
-  return arr.join('');
-}
-////////////////////////////////////////////////////////////////////////////////
-function reverse(str) {
-  return str
+function palindrome(str) {
+  const reversed = str
     .split('')
     .reverse()
     .join('');
-}
-///////////////////////////////////////////////////////////////////////
-function reverse(str) {
-  let reversed = '';
-  for (let char of str) {
-    reversed = char + reversed;
-  }
-  return reversed;
-}
-/////////////////////////////////////////////////////////////////////////
 
-function reverse(str) {
-  return str.split('').reduce((reversed, character) => {
-    return character + reversed;
-  }, '');
+  return str === reversed;
+}
+///////////////////////////////////////////////////////////
+
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
