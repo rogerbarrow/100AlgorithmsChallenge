43b6b4ed06b6664da9315d5bae31484c73197eaa
Roger Barrow <46138186+rogerbarrow@users.noreply.github.com>
2019-12-07 22:56:45 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-function reverseInt(n) {
-  const reversed = n
-    .toString()
+function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
+////////////////////////////////////////////////////////////////////////////////
+function reverse(str) {
+  return str
     .split('')
     .reverse()
     .join('');
+}
+///////////////////////////////////////////////////////////////////////
+function reverse(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+/////////////////////////////////////////////////////////////////////////
 
-  return parseInt(revesed);
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
 }
